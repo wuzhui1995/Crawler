2a9c27353f22700868ca13b39969911db4d9e40d
wuzhiwei <651383265@qq.com>
2017-06-15 12:18:18 +0800

    second

--- Scrping/Program.cs
+++ Scrping/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Scrping
 {
     class Program
     {
+        private static void extarurl(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream responsestream = response.GetResponseStream())
+                {
+                    using (StreamReader stream = new StreamReader(responsestream, Encoding.UTF8))
+                    {
+                        string strResponse = stream.ReadToEnd();
+                        long lgnResponse = response.ContentLength;
+                        string pattern = @"(http|ftp|https):\/\/([\w+\.\w+])+([a-zA-Z0-9\~\@\#\$\%\^\&\*\(\)\-\+\\\/\?\,\.\:\;\'\_\=]*)?";
+
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
